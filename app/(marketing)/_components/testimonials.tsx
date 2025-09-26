dd3da7d70d789bc1b1cab21720772ef38e98b341
= <=>
2025-09-25 22:56:47 -0400

    Add marketing landing page components, updated Next.js config, and added new assets

---
+++ app/(marketing)/_components/testimonials.tsx
@@ -0,0 +1,91 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+export default function Testimonials() {
+    return (
+        <section className="py-16 md:py-32">
+            <div className="mx-auto max-w-6xl space-y-8 px-6 md:space-y-16">
+                <div className="relative z-10 mx-auto max-w-xl space-y-6 text-center md:space-y-12">
+                    <h2 className="text-4xl font-medium lg:text-5xl">Build by makers, loved by thousand developers</h2>
+                    <p>Gemini is evolving to be more than just the models. It supports an entire to the APIs and platforms helping developers and businesses innovate.</p>
+                </div>
+
+                <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-rows-1">
+                   
+                    <Card className="">
+                        <CardContent className="h-full pt-6">
+                            <blockquote className="grid h-full grid-rows-[1fr_auto] gap-6">
+                                <p>"Tailus is really extraordinary and very practical, no need to break your head. A real gold mine."</p>
+
+                                <div className="grid grid-cols-[auto_1fr] items-center gap-3">
+                                    <Avatar className="size-12">
+                                        <AvatarImage
+                                            src="https://tailus.io/images/reviews/jonathan.webp"
+                                            alt="Jonathan Yombo"
+                                            height="400"
+                                            width="400"
+                                            loading="lazy"
+                                        />
+                                        <AvatarFallback>JY</AvatarFallback>
+                                    </Avatar>
+                                    <div>
+                                        <cite className="text-sm font-medium">Jonathan Yombo</cite>
+                                        <span className="text-muted-foreground block text-sm">Software Ingineer</span>
+                                    </div>
+                                </div>
+                            </blockquote>
+                        </CardContent>
+                    </Card>
+                    <Card className='md:col-span-2'>
+                        <CardContent className="h-full pt-6">
+                            <blockquote className="grid h-full grid-rows-[1fr_auto] gap-6">
+                                <p className="text-xl font-medium">"Great work on tailfolio template. This is one of the best personal website that I have seen so far!"</p>
+
+                                <div className="grid items-center gap-3 [grid-template-columns:auto_1fr]">
+                                    <Avatar className="size-12">
+                                        <AvatarImage
+                                            src="https://tailus.io/images/reviews/yucel.webp"
+                                            alt="Yucel Faruksahan"
+                                            height="400"
+                                            width="400"
+                                            loading="lazy"
+                                        />
+                                        <AvatarFallback>YF</AvatarFallback>
+                                    </Avatar>
+                                    <div>
+                                        <cite className="text-sm font-medium">Yucel Faruksahan</cite>
+                                        <span className="text-muted-foreground block text-sm">Creator, Tailkits</span>
+                                    </div>
+                                </div>
+                            </blockquote>
+                        </CardContent>
+                    </Card>
+                    <Card className="card variant-mixed">
+                        <CardContent className="h-full pt-6">
+                            <blockquote className="grid h-full grid-rows-[1fr_auto] gap-6">
+                                <p>"Great work on tailfolio template. This is one of the best personal website that I have seen so far!"</p>
+
+                                <div className="grid grid-cols-[auto_1fr] gap-3">
+                                    <Avatar className="size-12">
+                                        <AvatarImage
+                                            src="https://tailus.io/images/reviews/rodrigo.webp"
+                                            alt="Rodrigo Aguilar"
+                                            height="400"
+                                            width="400"
+                                            loading="lazy"
+                                        />
+                                        <AvatarFallback>YF</AvatarFallback>
+                                    </Avatar>
+                                    <div>
+                                        <p className="text-sm font-medium">Rodrigo Aguilar</p>
+                                        <span className="text-muted-foreground block text-sm">Creator, TailwindAwesome</span>
+                                    </div>
+                                </div>
+                            </blockquote>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </section>
+    )
+}
